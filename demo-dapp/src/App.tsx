54ef92bd6c1f63250b12db42e99f12961e671812
Thomas Lui <tlfit28@gmail.com>
2022-03-08 08:23:10 -0800

    Set token UI

--- demo-dapp/src/App.tsx
+++ demo-dapp/src/App.tsx
@@ -153,296 +153,24 @@ const App = () => {
     console.log("networks:", await wallet.getNetworks());
   };
 
-  const signMessage = async () => {
-    console.log("signing message...");
-    const signer = wallet.getSigner();
+  const createToken = async () => {
+    const signer = wallet.getSigner(); // select DefaultChain signer by default
 
-    const message = `Two roads diverged in a yellow wood,
-Robert Frost poet
-
-And sorry I could not travel both
-And be one traveler, long I stood
-And looked down one as far as I could
-To where it bent in the undergrowth;
-
-Then took the other, as just as fair,
-And having perhaps the better claim,
-Because it was grassy and wanted wear;
-Though as for that the passing there
-Had worn them really about the same,
-
-And both that morning equally lay
-In leaves no step had trodden black.
-Oh, I kept the first for another day!
-Yet knowing how way leads on to way,
-I doubted if I should ever come back.
-
-I shall be telling this with a sigh
-Somewhere ages and ages hence:
-Two roads diverged in a wood, and I—
-I took the one less traveled by,
-And that has made all the difference.`;
-
-    // sign
-    const sig = await signer.signMessage(message);
-    console.log("signature:", sig);
-
-    // validate
-    const isValid = await wallet.utils.isValidMessageSignature(
-      await wallet.getAddress(),
-      message,
-      sig,
-      await signer.getChainId()
-    );
-    console.log("isValid?", isValid);
-    if (!isValid) throw new Error("sig invalid");
-
-    // recover
-    // const walletConfig = await wallet.utils.recoverWalletConfigFromMessage(
-    //   await wallet.getAddress(),
-    //   message,
-    //   sig,
-    //   await signer.getChainId(),
-    //   sequenceContext
-    // )
-    // console.log('recovered walletConfig:', walletConfig)
-    // const match = walletConfig.address.toLowerCase() === (await wallet.getAddress()).toLowerCase()
-    // if (!match) throw new Error('recovery address does not match')
-    // console.log('address match?', match)
-  };
-
-  const signAuthMessage = async () => {
-    console.log("signing message on AuthChain...");
-    const signer = await wallet.getAuthSigner();
-
-    const message = "Hi there! Please sign this message, 123456789, thanks.";
-
-    // sign
-    const sig = await signer.signMessage(message, await signer.getChainId()); //, false)
-    console.log("signature:", sig);
-
-    // here we have sig from above method, on defaultChain ..
-    const notExpecting =
-      "0x0002000134ab8771a3f2f7556dab62622ce62224d898175eddfdd50c14127c5a2bb0c8703b3b3aadc3fa6a63dd2dc66107520bc90031c015aaa4bf381f6d88d9797e9b9f1c02010144a0c1cbe7b29d97059dba8bbfcab2405dfb8420000145693d051135be70f588948aeaa043bd3ac92d98057e4a2c0fbd0f7289e028f828a31c62051f0d5fb96768c635a16eacc325d9e537ca5c8c5d2635b8de14ebce1c02";
-    if (sig === notExpecting) {
-      throw new Error(
-        "this sig is from the DefaultChain, not what we expected.."
-      );
-    }
-
-    // validate
-    const isValid = await wallet.utils.isValidMessageSignature(
-      await wallet.getAddress(),
-      message,
-      sig,
-      await signer.getChainId()
-    );
-    console.log("isValid?", isValid);
-    if (!isValid) throw new Error("sig invalid");
-
-    console.log(
-      "is wallet deployed on mainnet?",
-      await wallet.isDeployed("mainnet")
-    );
-    console.log(
-      "is wallet deployed on matic?",
-      await wallet.isDeployed("polygon")
-    );
-
-    // recover
-    //
-    // TODO: the recovery here will not work, because to use addressOf(), we must have
-    // the init config for a wallet, wait for next index PR to come through then can fix this.
-    //
-    // TODO/NOTE: in order to recover this, the wallet needs to be updated on-chain,
-    // or we need the init config.. check if its deployed and updated?
-    // NOTE: this should work though, lets confirm it is deployed, and that the config is updated..
-    // const walletConfig = await wallet.utils.recoverWalletConfigFromMessage(
-    //   await wallet.getAddress(),
-    //   message,
-    //   sig,
-    //   await signer.getChainId()
-    // )
-
-    // const match = walletConfig.address.toLowerCase() === (await wallet.getAddress()).toLowerCase()
-    // // if (!match) throw new Error('recovery address does not match')
-    // console.log('address match?', match)
-  };
-
-  const signTypedData = async () => {
-    console.log("signing typedData...");
-
-    const typedData: sequence.utils.TypedData = {
-      domain: {
-        name: "Ether Mail",
-        version: "1",
-        chainId: await wallet.getChainId(),
-        verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
-      },
-      types: {
-        Person: [
-          { name: "name", type: "string" },
-          { name: "wallet", type: "address" },
-        ],
-      },
-      message: {
-        name: "Bob",
-        wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
-      },
-    };
-
-    const signer = wallet.getSigner();
-
-    const sig = await signer.signTypedData(
-      typedData.domain,
-      typedData.types,
-      typedData.message
-    );
-    console.log("signature:", sig);
-
-    // validate
-    const isValid = await wallet.utils.isValidTypedDataSignature(
-      await wallet.getAddress(),
-      typedData,
-      sig,
-      await signer.getChainId()
-    );
-    console.log("isValid?", isValid);
-    if (!isValid) throw new Error("sig invalid");
-
-    // recover
-    // const walletConfig = await wallet.utils.recoverWalletConfigFromTypedData(
-    //   await wallet.getAddress(),
-    //   typedData,
-    //   sig,
-    //   await signer.getChainId()
-    // )
-    // console.log('recovered walletConfig:', walletConfig)
-
-    // const match = walletConfig.address.toLowerCase() === (await wallet.getAddress()).toLowerCase()
-    // if (!match) throw new Error('recovery address does not match')
-    // console.log('address match?', match)
-  };
-
-  const signETHAuth = async () => {
-    const address = await wallet.getAddress();
-
-    const authSigner = await wallet.getAuthSigner();
-    console.log("AUTH CHAINID..", await authSigner.getChainId());
-    const authChainId = await authSigner.getChainId();
-
-    const proof = new Proof();
-    proof.address = address;
-    proof.claims.app = "wee";
-    proof.claims.ogn = "http://localhost:4000";
-    proof.setIssuedAtNow();
-    proof.setExpiryIn(1000000);
-
-    const messageTypedData = proof.messageTypedData();
-
-    const digest = sequence.utils.encodeTypedDataDigest(messageTypedData);
-    console.log("we expect digest:", digest);
-
-    const sig = await authSigner.signTypedData(
-      messageTypedData.domain,
-      messageTypedData.types,
-      messageTypedData.message
-    );
-    console.log("signature:", sig);
-
-    // validate
-    const isValid = await wallet.utils.isValidTypedDataSignature(
-      await wallet.getAddress(),
-      messageTypedData,
-      sig,
-      authChainId
-    );
-    console.log("isValid?", isValid);
-    if (!isValid) throw new Error("sig invalid");
-
-    // recover
-    // TODO/NOTE: in order to recover this, the wallet needs to be updated on-chain,
-    // or we need the init config.. check if its deployed and updated
-    // const walletConfig = await wallet.utils.recoverWalletConfigFromTypedData(
-    //   await wallet.getAddress(),
-    //   messageTypedData,
-    //   sig,
-    //   authChainId
-    // )
-
-    // console.log('recovered walletConfig:', walletConfig)
-    // const match = walletConfig.address.toLowerCase() === (await wallet.getAddress()).toLowerCase()
-    // // if (!match) throw new Error('recovery address does not match')
-    // console.log('address match?', match)
-  };
-
-  const sendETH = async (signer?: sequence.provider.Web3Signer) => {
-    signer = signer || wallet.getSigner(); // select DefaultChain signer by default
-
-    console.log(`Transfer txn on ${signer.getChainId()} chainId`);
-
-    // NOTE: on mainnet, the balance will be of ETH value
-    // and on matic, the balance will be of MATIC value
-    // const balance = await signer.getBalance()
-    // if (balance.eq(ethers.constants.Zero)) {
-    //   const address = await signer.getAddress()
-    //   throw new Error(`wallet ${address} has 0 balance, so cannot transfer anything. Deposit and try again.`)
-    // }
-
-    const toAddress = ethers.Wallet.createRandom().address;
-
-    const tx1: sequence.transactions.Transaction = {
-      delegateCall: false,
-      revertOnError: false,
-      gasLimit: "0x55555",
-      to: toAddress,
-      value: ethers.utils.parseEther("1.234"),
-      data: "0x",
-    };
-
-    const tx2: sequence.transactions.Transaction = {
-      delegateCall: false,
-      revertOnError: false,
-      gasLimit: "0x55555",
-      to: toAddress,
-      value: ethers.utils.parseEther("0.4242"),
-      data: "0x",
-    };
-
-    const provider = signer.provider;
-
-    console.log(
-      `balance of ${toAddress}, before:`,
-      await provider.getBalance(toAddress)
-    );
-
-    const txnResp = await signer.sendTransactionBatch([tx1, tx2]);
-    await txnResp.wait();
-
-    console.log(
-      `balance of ${toAddress}, after:`,
-      await provider.getBalance(toAddress)
-    );
-  };
-
-  const sendDAI = async (signer?: sequence.provider.Web3Signer) => {
-    signer = signer || wallet.getSigner(); // select DefaultChain signer by default
-
-    const toAddress = "0xA6AbC142278841F09DbC3CcA464fe3140e0f87b8";
-
-    const amount = ethers.utils.parseUnits("5", 18);
-
-    const daiContractAddress = "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa"; // (DAI address on Polygon)
+    const gameContractAddress = "0x02Ce10542B0f787438175c42F02111FF3Bbc0F8f"; // (Game address on Rinkeby)
 
     const tx: sequence.transactions.Transaction = {
       delegateCall: false,
       revertOnError: false,
       gasLimit: "0x55555",
-      to: daiContractAddress,
+      to: gameContractAddress,
       value: 0,
-      data: new ethers.utils.Interface(ERC_20_ABI).encodeFunctionData(
-        "transfer",
-        [toAddress, amount.toHexString()]
+      data: new ethers.utils.Interface(GAME_ABI).encodeFunctionData(
+        "createToken",
+        [
+          1,
+          "ipfs://bafkreieggymjltgxgwyxulsuxor7hhy62cgrbq6vsna3cmroupm5jpg7mi",
+          5,
+        ]
       ),
     };
 
@@ -450,18 +178,10 @@ And that has made all the difference.`;
     await txnResp.wait();
   };
 
-  const sendETHSidechain = async () => {
-    // const signer = wallet.getSigner(137)
-    // Select network that isn't the DefaultChain..
-    const networks = await wallet.getNetworks();
-    const n = networks.find((n) => n.isAuthChain);
-    sendETH(wallet.getSigner(n));
-  };
-
   const claim1155Tokens = async (signer?: sequence.provider.Web3Signer) => {
     signer = signer || wallet.getSigner(); // select DefaultChain signer by default
 
-    const gameContractAddress = "0x1edbf67231c589f1d7f119f691dc2c0811602af7"; // (Game address on Rinkeby)
+    const gameContractAddress = "0x02Ce10542B0f787438175c42F02111FF3Bbc0F8f"; // (Game address on Rinkeby)
 
     const tx: sequence.transactions.Transaction = {
       delegateCall: false,
@@ -470,7 +190,7 @@ And that has made all the difference.`;
       to: gameContractAddress,
       value: 0,
       data: new ethers.utils.Interface(GAME_ABI).encodeFunctionData("claim", [
-        0,
+        1,
       ]),
     };
 
@@ -509,19 +229,11 @@ And that has made all the difference.`;
         <Button onClick={() => getWalletState()}>Get Wallet State</Button>
       </Group>
 
-      <Group label="Signing" layout="grid">
-        <Button onClick={() => signMessage()}>Sign Message</Button>
-        <Button onClick={() => signTypedData()}>Sign TypedData</Button>
-        <Button onClick={() => signAuthMessage()}>
-          Sign Message on AuthChain
-        </Button>
-        <Button onClick={() => signETHAuth()}>Sign ETHAuth</Button>
+      <Group label="Create token" layout="grid">
+        <Button onClick={() => createToken()}>Create token</Button>
       </Group>
 
       <Group label="Transactions" layout="grid">
-        <Button onClick={() => sendETH()}>Send on DefaultChain</Button>
-        <Button onClick={() => sendETHSidechain()}>Send on AuthChain</Button>
-        <Button onClick={() => sendDAI()}>Send DAI</Button>
         <Button onClick={() => claim1155Tokens()}>Claim ERC-1155 Tokens</Button>
         {/* <Button onClick={() => sendBatchTransaction()}>Send Batch Txns</Button> */}
       </Group>
